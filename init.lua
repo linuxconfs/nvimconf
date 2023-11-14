65a0b3060c47bcec0407bff2ebe7b192041c7875
zhangddjs <1005155946@qq.com>
2023-11-14 14:24:34 +0800

    improve config
    temp
    
    improve config

--- init.lua
+++ init.lua
@@ -27,6 +27,10 @@ end, 10000)
 
 vim.api.nvim_set_keymap("v", "p", '"0p', { noremap = true })
 vim.api.nvim_set_keymap("n", "<space>ge", "<cmd>Lspsaga diagnostic_jump_next<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "gf", "<cmd>GoFillStruct<CR>", { noremap = true })
 vim.api.nvim_set_hl(0, "@comment", { fg = "green" })
 
 -- vim.o.tabstop = 4
+vim.api.nvim_exec([[
+  autocmd FileType php setlocal tabstop=4 shiftwidth=4 expandtab
+]], false)
--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,3 +1,10 @@
 return {
   { "rcarriga/nvim-notify", enabled = false },
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = function(_, opts)
+      opts.autoformat = false
+    end,
+  },
 }
