47024403308cff188bff5eb7097f44cdd8b16ca2
zhangddjs <1005155946@qq.com>
2024-12-29 20:32:45 +0800

    fix sessions-len issue

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -1,18 +1,12 @@
-return { 
+return {
   {
-    'rmagatti/auto-session',
-    config = function()
-      require("auto-session").setup {
-        log_level = "error",
-        auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
-      }
-    end
+    "rmagatti/auto-session",
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      -- log_level = 'debug',
+    },
   },
-  {
-    'rmagatti/session-lens',
-    requires = {'rmagatti/auto-session', 'nvim-telescope/telescope.nvim'},
-    config = function()
-      require('session-lens').setup({--[[your custom config--]]})
-    end
-  }
 }
