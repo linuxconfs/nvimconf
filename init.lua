722f6d256aa1f57a81734b435319df85c435caad
zhangddjs <1005155946@qq.com>
2025-01-02 11:10:50 +0800

    remove c-m to zoom mode cuz enter already can do

--- init.lua
+++ init.lua
@@ -48,8 +48,8 @@ vim.cmd([[
 -- vafaf: expand text block
 
 -- DBUI
-local keymap = vim.keymap
-keymap.set("n", "<localleader>S", ":normal vip<CR><PLUG>(DBUI_ExecuteQuery)", { buffer = true })
+-- local keymap = vim.keymap
+-- keymap.set("n", "<localleader>S", ":normal vip<CR><PLUG>(DBUI_ExecuteQuery)", { buffer = true })
 -- keymap.set('n', '<leader>F', ':%!sql-formatter-cli .<CR>', { buffer = true })
 -- keymap.set('n', '<leader>f', ':normal vip<CR>:!sql-formatter-cli<CR>', { buffer = true })
 
