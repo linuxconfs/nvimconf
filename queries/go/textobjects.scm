f0a0b538cfcf4c948f4a40a875e25c8beb2f66da
zhangddjs <1005155946@qq.com>
2025-02-25 14:30:38 +0800

    improve treesitter textobj

--- queries/go/textobjects.scm
+++ queries/go/textobjects.scm
@@ -2,7 +2,7 @@
 ;; 选中整个函数，并包括上下的空行
 ; outer function textobject
 (function_declaration body: (_)? @function.outercustome
-  (#offset! @function.outercustome -1 0 1 0))
+  (#offset! @function.outercustome -2 0 0 0))
 ; (function_declaration body: (_)? @function.outer
 ;   (#offset! @function.outer -1 0 1 0))
 
@@ -10,10 +10,10 @@
 ; outer function literals
 (func_literal (_)?
   @function.outercustome
-  (#offset! @function.outercustome -1 0 1 0))
+  (#offset! @function.outercustome -2 0 0 0))
 
 ; method as outer function textobject
 (method_declaration
   body: (block)?
   @function.outercustome
-  (#offset! @function.outercustome -1 0 1 0))
+  (#offset! @function.outercustome -2 0 0 0))
