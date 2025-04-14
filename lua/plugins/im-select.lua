d4a2963a461dcc128e91dc462a2a4c086986904d
zhangddjs <1005155946@qq.com>
2025-04-15 00:10:01 +0800

    im-select feature

---
+++ lua/plugins/im-select.lua
@@ -0,0 +1,9 @@
+return {
+  "keaising/im-select.nvim",
+  config = function()
+    require("im_select").setup({
+      default_im_select = "com.apple.keylayout.ABC",
+      default_command = "macism",
+    })
+  end,
+}
