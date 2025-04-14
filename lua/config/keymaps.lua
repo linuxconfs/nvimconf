afb60a411cf12f0fb22c6e6a9cdaabf68c885e76
zhangddjs <1005155946@qq.com>
2025-04-14 22:33:34 +0800

    improve keymaps
    improve

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,24 +1,25 @@
--- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local map = vim.keymap.set
+local opts = { noremap = true, unique = true, silent = true }
 
-map("v", "p", '"0p', { noremap = true })
-map("x", "q", [[:<C-u>lua MiniSurround.add('visual')<CR>]], { noremap = true, silent = true })
+map("v", "p", '"0p', opts)
+map("x", "q", [[:<C-u>lua MiniSurround.add('visual')<CR>]], opts)
 map("x", "<leader>gl", ":Gllog<CR>", { noremap = true, silent = true })
-map("n", "<space>db", "<cmd>DBUI<CR>", { noremap = true, silent = true })
-map("n", "gf", "<cmd>GoFillStruct<CR>", { noremap = true, silent = true })
-map("n", "g<S-r>", "<cmd>GoRun<CR>", { noremap = true, silent = true })
-map("n", "gt", "<cmd>GoTestFunc -v<CR>", { noremap = true, silent = true })
-map("n", "gi", "<cmd>GoImports<CR>", { noremap = true, silent = true })
-map("n", "g<S-s>", "<cmd>GoStop<CR>", { noremap = true, silent = true })
-map("i", "<c-a>", "<c-o>^", { noremap = true })
-map("i", "<c-e>", "<c-o>$", { noremap = true })
-map("i", "<c-j>", "<c-o>o", { noremap = true })
-map("n", "<leader>gb", ":Git blame<CR>", { noremap = true, silent = true })
-map("n", "<leader>gl", ":Git log --graph --oneline<CR>", { noremap = true, silent = true })
-map("n", "gp", "`[v`]", { noremap = true, silent = true })
-map("n", "<leader>m", ":MaximizerToggle<CR>", { noremap = true, silent = true })
+map("n", "<space>db", "<cmd>DBUI<CR>", opts)
+map("n", "gf", "<cmd>GoFillStruct<CR>", opts)
+map("n", "g<S-r>", "<cmd>GoRun<CR>", opts)
+map("n", "gt", "<cmd>GoTestFunc -v<CR>", opts)
+map("n", "gi", "<cmd>GoImports<CR>", opts)
+map("n", "g<S-s>", "<cmd>GoStop<CR>", opts)
+map("i", "<c-a>", "<c-o>^", opts)
+map("i", "<c-e>", "<c-o>$", opts)
+map("i", "<c-j>", "<c-o>o", opts)
+-- map("n", "<leader>gb", ":Git blame<CR>", opts)
+-- map("n", "<leader>gl", ":Git log --graph --oneline<CR>", { noremap = true, silent = true })
+map("x", "<Leader>fc", "<cmd>FzfLua git_bcommits<CR>", opts)
+map("n", "gp", "`[v`]", opts)
+map("n", "<leader>m", ":MaximizerToggle<CR>", opts)
 map("n", "<leader>ss", ":SessionSearch<CR>", { noremap = true, silent = true })
 map("n", "<leader>hs", 'q:i%g//lua vim.api.nvim_buf_add_highlight(0, 0, "Search", vim.fn.line(".")-1, 0, -1)<esc>0fgla')
 
@@ -29,9 +30,9 @@ map("n", "<leader>W", "<PLUG>(DBUI_SaveQuery)")
 require("config.accordion").maximize():map("<leader>wm")
 
 -- treesitter switch argument
-map("i", "<c-l>", "<ESC>:normal ]avia<c-g><CR>", { noremap = true, silent = true })
+map("i", "<c-l>", "<ESC>:normal ]avia<c-g><CR>", opts)
 -- map("n", "<tab><tab>", ":normal ]avia<c-g><CR>", { noremap = true, silent = true }) -- will conflict with <c-i>
-map("s", "<c-l>", "<ESC>:normal ]avia<c-g><CR>", { noremap = true, silent = true })
+map("s", "<c-l>", "<ESC>:normal ]avia<c-g><CR>", opts)
 -- vim.api.nvim_set_keymap("i", "<tab><tab>", ':normal ]avia<CR>', { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap("n", "<tab><tab>", ':normal ]avia<CR>', { noremap = true, silent = true })
 
